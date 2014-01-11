e512ae027c3310ec2a45e0c129422a1e9d806587
Eyad Makki <eimakki@umich.edu>
2014-01-11 02:12:40 -0500

    Added min difference check

--- python/util.py
+++ python/util.py
@@ -29,6 +29,9 @@ def getLeadCard(msg):
 def respondToPlay(msg, theirCard):
 # if there is no highest card, it returns the lowest card
 	card = min(msg["state"]["hand"])
+	if (theirCard - min) >= 5 and msg["state"]["their_tricks"] < 2:
+		return card
+
 	for a in msg["state"]["hand"]:
 		if card < theirCard and a == theirCard:
 			card = theirCard
