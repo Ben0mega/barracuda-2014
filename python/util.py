fad34141e9d8635afb8d316bfe74725df60d8d22
Thomas Dorsey <twdorsey@login.engin.umich.edu>
2014-01-11 09:43:06 -0500

    fixed bugs

--- python/util.py
+++ python/util.py
@@ -67,30 +67,13 @@ def shouldStartChallenge(msg, deck):
          return True
     #when behind, dark shrine
     if behind(msg):
-<<<<<<< HEAD
-        #TODO: make this a ratio with regards to how far each player is from winning
-        if calculateHandScore(msg, 8):
-=======
         #TODO: make this a ratio with regards to how far each player is from winning
         if calculateHandScore(msg, deck) > 8:
->>>>>>> 2d3af12b327fa85b93a70c50a3dfdbf779155889
            return True 
     
     #if len(msg["state"]["hand"]) == 1:
         #return True
 
-<<<<<<< HEAD
-    if not isLastCard(msg) and calculateHandScore(msg, 9.5):
-        if msg["state"]["their_tricks"] == (neededToWin(msg)-1) and "card" in msg["state"].keys() and msg["state"]["card"] > max(msg["state"]["hand"]):
-            return False
-        return True
-
-    if (msg["state"]["your_points"] - msg["state"]["their_points"]) > neededToWin(msg):
-        if calculateHandScore(msg, 9.7):
-            return True
-    if msg["state"]["your_points"] == 9:
-        if calculateHandScore(msg, 11):
-=======
     if not isLastCard(msg) and calculateHandScore(msg, deck) > 9.5:
         if msg["state"]["their_tricks"] == 2 and "card" in msg["state"].keys() and msg["state"]["card"] > max(msg["state"]["hand"]):
             return False
@@ -107,16 +90,11 @@ def shouldStartChallenge(msg, deck):
 
     if (msg["state"]["your_points"] - msg["state"]["their_points"]) > neededToWin(msg):
         if calculateHandScore(msg, deck) > 9.7:
->>>>>>> 2d3af12b327fa85b93a70c50a3dfdbf779155889
             return True
     if msg["state"]["their_tricks"] < neededToWin(msg):    # can you win the challenge?
         if msg["state"]["their_tricks"] == (neededToWin(msg)-1) and "card" in msg["state"].keys() and msg["state"]["card"] > max(msg["state"]["hand"]):
             return False
-<<<<<<< HEAD
-        if calculateHandScore(msg, 10.2) and msg["state"]["their_tricks"]+count_num_card(msg, 7) < neededToWin(msg):
-=======
         if calculateHandScore(msg, deck) > 10.2 and msg["state"]["their_tricks"]+count_num_card(msg, 7) < 3:
->>>>>>> 2d3af12b327fa85b93a70c50a3dfdbf779155889
             return True
     return False
 
@@ -137,11 +115,7 @@ def shouldAcceptChallenge(msg, deck):
            return True 
     if msg["state"]["your_tricks"]+count_num_card(msg, 12) >=3:
          return True
-<<<<<<< HEAD
-    if calculateHandScore(msg, 10.2) and len(msg["state"]["hand"]) > 1:
-=======
     if calculateHandScore(msg, deck) > 10.2 and len(msg["state"]["hand"]) > 1:
->>>>>>> 2d3af12b327fa85b93a70c50a3dfdbf779155889
         return True
     if aheadByEnoughTricks(msg):
         if not isLastCard(msg) and calculateHandScore(msg, deck) > 11: #averageHandValue(msg, 11):
