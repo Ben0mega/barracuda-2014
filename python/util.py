5a135c99c81addc67c9ec5e40250bbad7ff14660
Eyad Makki <eimakki@umich.edu>
2014-01-11 09:40:43 -0500

    updated constants for risks when we're behind

--- python/util.py
+++ python/util.py
@@ -66,27 +66,27 @@ def shouldStartChallenge(msg, deck):
     #when behind, dark shrine
     if behind(msg):
 		#TODO: make this a ratio with regards to how far each player is from winning
-        if averageHandValue(msg, 8):
+        if calculateHandScore(msg, 8):
            return True 
     
     #if len(msg["state"]["hand"]) == 1:
         #return True
 
-    if not isLastCard(msg) and averageHandValue(msg, 9.5):
+    if not isLastCard(msg) and calculateHandScore(msg, 9.5):
         if msg["state"]["their_tricks"] == (neededToWin(msg)-1) and "card" in msg["state"].keys() and msg["state"]["card"] > max(msg["state"]["hand"]):
             return False
         return True
 
-    if (msg["state"]["your_points"] - msg["state"]["their_points"]) > 3:
-        if averageHandValue(msg, 9.7):
+    if (msg["state"]["your_points"] - msg["state"]["their_points"]) > neededToWin(msg):
+        if calculateHandScore(msg, 9.7):
             return True
     if msg["state"]["your_points"] == 9:
-        if averageHandValue(msg, 11):
+        if calculateHandScore(msg, 11):
             return True
     if msg["state"]["their_tricks"] < neededToWin(msg):    # can you win the challenge?
         if msg["state"]["their_tricks"] == (neededToWin(msg)-1) and "card" in msg["state"].keys() and msg["state"]["card"] > max(msg["state"]["hand"]):
             return False
-        if averageHandValue(msg, 10.2) and msg["state"]["their_tricks"]+count_num_card(msg, 7) < neededToWin(msg):
+        if calculateHandScore(msg, 10.2) and msg["state"]["their_tricks"]+count_num_card(msg, 7) < neededToWin(msg):
             return True
     return False
 
@@ -107,7 +107,7 @@ def shouldAcceptChallenge(msg, deck):
            return True 
     if msg["state"]["your_tricks"]+count_num_card(msg, 12) >=3:
          return True
-    if averageHandValue(msg, 10.2) and len(msg["state"]["hand"]) > 1:
+    if calculateHandScore(msg, 10.2) and len(msg["state"]["hand"]) > 1:
         return True
     if aheadByEnoughTricks(msg):
         if not isLastCard(msg) and calculateHandScore(msg, deck) > 11: #averageHandValue(msg, 11):
