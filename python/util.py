4cb30f4c024330c5b6755b12150d02006dec6136
Eyad Makki <eimakki@umich.edu>
2014-01-11 10:37:08 -0500

    updated constants for risks when we're behind

--- python/util.py
+++ python/util.py
@@ -96,7 +96,7 @@ def shouldStartChallenge(msg, deck):
     if msg["state"]["their_tricks"] < neededToWin(msg):    # can you win the challenge?
         if msg["state"]["their_tricks"] == (neededToWin(msg)-1) and "card" in msg["state"].keys() and msg["state"]["card"] > max(msg["state"]["hand"]):
             return False
-        if calculateHandScore(msg, deck) > 10.2 and msg["state"]["their_tricks"]+count_num_card(msg, 7) < 3:
+        if calculateHandScore(msg, deck) > 10.2 and msg["state"]["their_tricks"]+count_num_card(msg, 7) < neededToWin(msg):
             return True
     return False
 
@@ -115,9 +115,9 @@ def shouldAcceptChallenge(msg, deck):
     if behind(msg):
         if calculateHandScore(msg, deck) > 10: #averageHandValue(msg, 9.4):
            return True 
-    if msg["state"]["your_tricks"]+count_num_card(msg, 11) >=neededToWin(msg):
+    if msg["state"]["your_tricks"]+count_num_card(msg, 11) >=(neededToWin(msg)-msg["state"]["your_tricks"]):
          return True
-    if msg["state"]["your_points"] == 9 and count_num_card(msg, 11) >= 2:
+    if msg["state"]["your_points"] == 9 and count_num_card(msg, 11) >= (neededToWin(msg)-msg["state"]["your_tricks"]):
         return True
     if isLastCard(msg):
         return False
