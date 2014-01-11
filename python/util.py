2f78831a348fc0af362d760df31f25ec9ceaadd1
Eyad Makki <eimakki@umich.edu>
2014-01-11 10:16:37 -0500

    updated constants for risks when we're behind

--- python/util.py
+++ python/util.py
@@ -131,7 +131,7 @@ def shouldAcceptChallenge(msg, deck):
             return False
         if calculateHandScore(msg, deck) > 10.2 and msg["state"]["their_tricks"]+count_num_card(msg, 7) < 3:
             return True
-        if msg["state"]["your_points"] == 9 and calculateHandScore(msg, deck) > 10: #averageHandValue(msg, 9.4):
+        if msg["state"]["your_points"] == 9 and calculateHandScore(msg, deck) > 10.5: #averageHandValue(msg, 9.4):
             return True
     return False
 
